3f2637f2df90573fffebff13ee2ecb0c3dd383fd
root <root@docker01.aihchhe2njoejafzunruedvkah.ax.internal.cloudapp.net>
2020-03-13 10:58:07 +0000

    app.py file changed

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world ():
-    return 'Hey, we have Flask in a Docker container!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/")
+def hello():
+    return "Hello, World!"
