b4440d4a8f5e98248555adf5bb57b976b9430408
ahmed akbarov <lab_admin02@epmcseclab.com>
2020-03-23 11:23:36 +0300

    several changes

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#    return 'Hello, World! ahmed'
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World! ahmed'
+def index():
+   return render_template(‘index.html’, name = user)
+if __name__ == '__main__':
+   app.run(debug = True)
