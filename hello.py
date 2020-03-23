36d0822de219eeea4261ecf7f29932959b4b32ce
ahmed akbarov <lab_admin02@epmcseclab.com>
2020-03-23 10:23:50 +0300

    added index and html files:

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World! Hey My name is Ahmed'
+    @app.route("/")
+def index():
+    return render_template("index.html")
