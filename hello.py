739ad7225b0198a317bf93d8b41933ac86dd8e2b
ahmed akbarov <lab_admin02@epmcseclab.com>
2020-03-23 12:00:56 +0300

    added gitignore

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#    return 'Hello, World! ahmed'
-
 @app.route('/')
-def index():
-   return render_template(‘index.html’, name = user)
-if __name__ == '__main__':
-   app.run(debug = True)
+def hello_world():
+   return 'Hello, World! ahmed'
