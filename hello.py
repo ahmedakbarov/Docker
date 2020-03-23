3f4c1c5f89a80fda9f41a0c38575404bcff27f1a
ahmed akbarov <lab_admin02@epmcseclab.com>
2020-03-23 10:48:26 +0300

    several changes

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
-    
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/")
+def elements():
+    return render_template("elements.html")
+@app.route("/")
+def generic():
+    return render_template("generic.html")
+if __name__ == '__main__':
+   app.run(debug = True)
