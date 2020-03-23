8de24d352328457359f02eadd07e36f46cfbdfc5
ahmed akbarov <lab_admin02@epmcseclab.com>
2020-03-23 10:57:53 +0300

    several changes

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM python:2.7
+FROM python:latest
 RUN pip install FLask
 COPY . /
 ENV FLASK_APP=hello.py
--- hello.py
+++ hello.py
@@ -1,13 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template("index.html")
-@app.route("/")
-def elements():
-    return render_template("elements.html")
-@app.route("/")
-def generic():
-    return render_template("generic.html")
-if __name__ == '__main__':
-   app.run(debug = True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! ahmed'
